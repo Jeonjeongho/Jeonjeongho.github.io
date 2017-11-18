a054da8c8198c2c38c57d4a51ec2699bf33a4856
jeonjeongho <wjswjdgh757@naver.com>
2017-11-18 22:04:43 +0900

    test

---
+++ asset/js/sports-toto.js
@@ -0,0 +1,65 @@
+webpackJsonp([0],{
+
+/***/ 329:
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(330);
+module.exports = __webpack_require__(332);
+
+
+/***/ }),
+
+/***/ 330:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_url_search_params__ = __webpack_require__(125);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_url_search_params___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_url_search_params__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__web_common__ = __webpack_require__(331);
+
+
+
+
+var appMethods = {};
+
+var appInit = function appInit() {
+    /*
+    const appName = $('body').attr('id').replace(/\w+-/, '');
+    const appCommon = appName.split('-')[0];
+    const appMethod = appName.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
+    const params = new URLSearchParams(location.search);
+      [appCommon, appMethod].forEach(method => appMethods[method] && appMethods[method](params));
+    */
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    appInit();
+    Object(__WEBPACK_IMPORTED_MODULE_1__web_common__["a" /* default */])();
+
+    // 추가 함수
+});
+
+/***/ }),
+
+/***/ 331:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function($) {var Common = function Common() {
+    $("body").css({ "background": "#ccc" });
+};
+
+/* harmony default export */ __webpack_exports__["a"] = (Common);
+/* WEBPACK VAR INJECTION */}.call(__webpack_exports__, __webpack_require__(124)))
+
+/***/ }),
+
+/***/ 332:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+
+},[329]);
